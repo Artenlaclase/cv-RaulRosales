ea9472195f62daa3f28b7762ecb9031f1d81549f
Artenlaclase <artenlaclase@gmail.com>
2025-03-04 23:35:29 -0300

    re instalación

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['localhost'], // añade dominios de imágenes si es necesario
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
