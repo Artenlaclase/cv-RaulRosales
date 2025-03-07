5a580fe5ac3dad7b99a1def9c018db8418eb6c4b
Artenlaclase <artenlaclase@gmail.com>
2025-03-07 00:52:09 -0300

    correciones about

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -59,17 +59,13 @@ export default function About() {
 
         <Box sx={{ mx: "auto", maxWidth: "48rem", py: 6 }}>
           <Typography variant="body1" paragraph sx={{ mb: 3, fontSize: "1.125rem" }}>
-          Soy desarrollador Full Stack con más de 3 años de experiencia en el desarrollo de aplicaciones web modernas, iniciando con WordPress y Joomla. Mi pasión por la tecnología comenzó desde temprana edad, y he dedicado mi carrera profesional a perfeccionar mis habilidades en el desarrollo frontend y backend.
-          </Typography>
+          Desarrollador Full Stack con más de 3 años de experiencia en el desarrollo de aplicaciones web modernas, comenzando con WordPress y Joomla. La pasión por la tecnología nació desde temprana edad, y a lo largo de mi carrera he perfeccionado mis habilidades tanto en el desarrollo frontend como backend.          </Typography>
           <Typography variant="body1" paragraph sx={{ mb: 3, fontSize: "1.125rem" }}>
-          Además, soy Magíster en Educación y Profesor de Artes Visuales, titulado en la Universidad Metropolitana de Ciencias de la Educación (UMCE), con un postítulo como Profesor Especialista en Educación Tecnológica. Cuento con 20 años de experiencia en centros educativos, donde he diseñado e implementado asignaturas como Artes Visuales, Tecnología, y talleres de Audiovisual, Proyectos Tecnológicos y Programación.
-          </Typography>
+          Cuento con un Magíster en Educación y soy Profesor de Artes Visuales, titulado en la Universidad Metropolitana de Ciencias de la Educación (UMCE), además de tener un postítulo como Profesor Especialista en Educación Tecnológica. Con 20 años de experiencia en centros educativos, he diseñado e implementado asignaturas como Artes Visuales, Tecnología, y talleres de Audiovisual, Proyectos Tecnológicos y Programación.          </Typography>
           <Typography variant="body1" paragraph sx={{ mb: 3, fontSize: "1.125rem" }}>
-          Tengo experiencia como administrador y programador de sitios web para instituciones educativas, optimizando su funcionalidad y accesibilidad. Durante tres años, fui responsable del proyecto Enlaces del Ministerio de Educación en el colegio donde realizaba clases, configurando y administrando laboratorios de computación e integrando tecnología educativa para mejorar la enseñanza y el aprendizaje.
-          </Typography>
+          Mi trayectoria incluye la administración y programación de sitios web para instituciones educativas, enfocándome en la optimización de su funcionalidad y accesibilidad. Durante tres años, fui responsable del proyecto Enlaces del Ministerio de Educación en el colegio donde trabajaba, configurando y administrando laboratorios de computación e integrando tecnología educativa para mejorar la enseñanza y el aprendizaje.          </Typography>
           <Typography variant="body1" sx={{ fontSize: "1.125rem" }}>
-          Soy un profesional con amplia experiencia en educación y tecnología educativa, especializado en la integración de plataformas de aprendizaje virtual (LMS) y en el diseño instruccional para entornos digitales. Mis habilidades incluyen la capacitación docente y el liderazgo en la implementación de proyectos tecnológicos, gestionando la modernización de espacios educativos mediante TIC y metodologías innovadoras para la enseñanza y el aprendizaje.
-          </Typography>
+          Mi enfoque profesional se basa en la integración de plataformas de aprendizaje virtual (LMS) y el diseño instruccional para entornos digitales. Mis competencias abarcan la capacitación docente y el liderazgo en la implementación de proyectos tecnológicos, gestionando la modernización de espacios educativos mediante TIC y metodologías innovadoras para la enseñanza y el aprendizaje.          </Typography>
         </Box>
       </Container>
     </Box>
